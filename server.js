bfa1c1715571c5a35014a74186745a0f41174c1a
Brian <galyenb@vcu.edu>
2023-11-01 11:42:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const termData = require("./db/terms.json");
+
+const app = express();
+const PORT = 3001;
+//routes
+////get notes
+
+////posts new note
+
+app.listen(PORT, () => {});
