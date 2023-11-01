147fe6e51549ee57bc79eb18962160341aa21fb7
Brian <galyenb@vcu.edu>
2023-11-01 11:59:16 -0400

    set up basic routes

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const path = require("path");
-const termData = require("./db/terms.json");
+const notesData = require("./db/db.json");
 
 const app = express();
 const PORT = 3001;
+
+// Middleware to default
+app.use(express.static("public"));
+
 //routes
+//default redundant route
+app.get("/", (req, res) => {
+  res.send(`Please Enter A Request`);
+});
+
 ////get notes
+app.get("/", (req, res) => {
+  res.send(`Please Enter A Request`);
+});
 
 ////posts new note
+app.post("/api/notes", (req, res) => {});
 
-app.listen(PORT, () => {});
+////Delete Note
+app.delete("/api/notes:id", (req, res) => {
+  const noteId = "";
+});
+
+//// Run server and listen for requests
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
